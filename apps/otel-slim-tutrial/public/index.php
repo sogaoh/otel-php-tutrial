2abaa8d05822eada4648617f60241a2ff9694712
Hisashi SOGA <sogaoh@ant-in-giant.llc>
2024-05-11 00:51:03 +0900

    Add manual instrumentation のところまで

--- apps/otel-slim-tutrial/public/index.php
+++ apps/otel-slim-tutrial/public/index.php
@@ -1,4 +1,6 @@
 <?php
+
+use OpenTelemetry\API\Globals;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -6,11 +8,19 @@ use Slim\Factory\AppFactory;
 //require __DIR__ . '/vendor/autoload.php';
 require '/workdir/vendor/autoload.php';
 
+$tracer = Globals::tracerProvider()->getTracer('demo');
+
 $app = AppFactory::create();
 
-$app->get('/rolldice', function (Request $request, Response $response) {
+$app->get('/rolldice', function (Request $request, Response $response) use ($tracer) {
+    $span = $tracer
+        ->spanBuilder('manual-span')
+        ->startSpan();
     $result = random_int(1,6);
     $response->getBody()->write(strval($result));
+    $span
+        ->addEvent('rolled dice', ['result' => $result])
+        ->end();
     return $response;
 });
 
