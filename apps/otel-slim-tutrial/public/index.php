0006ba4175d0faca9d9d6b6991de5b043daaead2
Hisashi SOGA <sogaoh@ant-in-giant.llc>
2024-05-11 00:55:03 +0900

    Logging まで

--- apps/otel-slim-tutrial/public/index.php
+++ apps/otel-slim-tutrial/public/index.php
@@ -1,26 +1,29 @@
 <?php
 
+use Monolog\Logger;
 use OpenTelemetry\API\Globals;
+use OpenTelemetry\Contrib\Logs\Monolog\Handler;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LogLevel;
 use Slim\Factory\AppFactory;
 
 //require __DIR__ . '/vendor/autoload.php';
 require '/workdir/vendor/autoload.php';
 
-$tracer = Globals::tracerProvider()->getTracer('demo');
+$loggerProvider = Globals::loggerProvider();
+$handler = new Handler(
+    $loggerProvider,
+    LogLevel::INFO
+);
+$monolog = new Logger('otel-php-monolog', [$handler]);
 
 $app = AppFactory::create();
 
-$app->get('/rolldice', function (Request $request, Response $response) use ($tracer) {
-    $span = $tracer
-        ->spanBuilder('manual-span')
-        ->startSpan();
+$app->get('/rolldice', function (Request $request, Response $response) use ($monolog) {
     $result = random_int(1,6);
     $response->getBody()->write(strval($result));
-    $span
-        ->addEvent('rolled dice', ['result' => $result])
-        ->end();
+    $monolog->info('dice rolled', ['result' => $result]);
     return $response;
 });
 
